a396fe1a0d580289ead7602dc980758c88778a29
DKTNS <zigindmitry@gmail.com>
2024-06-19 16:20:04 +0300

    Фильтры раскрываются. без стилей.

--- skypro-music/src/components/filters/filterItem/filterItem.tsx
+++ skypro-music/src/components/filters/filterItem/filterItem.tsx
@@ -2,22 +2,32 @@ import classNames from "classnames";
 import styles from "./filterItem.module.css";
 import { FilteItemType } from "@/types";
 
-
-export default function FilterItem({handleFilterClick, title, list}:FilteItemType) {
-
+export default function FilterItem({
+  handleFilterClick,
+  title,
+  list,
+  isOpened
+}: FilteItemType) {
   return (
-<>    <div onClick={() => handleFilterClick(title)}
-    className={classNames(
-      styles.filterButton,
-      styles.buttonAuthor,
-      styles.btnText
-    )}
-  >
-    {title} {/* исполнителю */}
-  </div>
-  {/* подтягиваем список(мапим) и передаем его в li строку как item
+    <>
+
+      <div
+        onClick={() => handleFilterClick(title)}
+        className={classNames(
+          styles.filterButton,
+          styles.buttonAuthor,
+          styles.btnText
+        )}
+      >
+        {title} {/* исполнителю */}
+      </div>
+      {/* подтягиваем список(мапим) и передаем его в li строку как item
   это будет выпадающий спсиок. */}
-    <ul>{list.map((item) => <li key={item}>{item}</li>)}</ul>
-  </>
+      {isOpened && (<ul>
+        {list.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>)}
+    </>
   );
 }
